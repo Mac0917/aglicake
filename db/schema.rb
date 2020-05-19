ff27fbc8a886be4b90787eeaa1a83b5cff243744
Mac <mac@makkunoMacBook-Pro.local>
2020-05-19 19:24:01 +0900

    修正しましたよ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_133433) do
+ActiveRecord::Schema.define(version: 2020_05_19_094523) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_05_16_133433) do
     t.integer "status", default: 0
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "member_id"
+    t.string "delively_address"
+    t.string "delively_name"
+    t.string "post_number"
   end
 
 end
