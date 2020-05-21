54eb848f1519c80a92ba25660ab0d73deb532311
Mac <mac@makkunoMacBook-Pro.local>
2020-05-21 22:26:30 +0900

    ログイン機能について修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,20 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_member!, except: [:top]
-	def index
-  	@items = Item.page(params[:page]).reverse_order
-    @genres = Genre.all
+  
+  def index
+    if member_signed_in? 
+        @items = Item.page(params[:page]).reverse_order
+        @genres = Genre.all
+        @member = Member.find(current_member.id)
+        if @member.status == "無効会員"
+            reset_session
+            flash[:notice] = "このメールアドレスは使用できません"
+            redirect_to root_path
+        end
+    else
+        @items = Item.page(params[:page]).reverse_order
+        @genres = Genre.all
+    end
   end
-
-
 
   def show
       @item = Item.find(params[:id])
@@ -13,16 +22,10 @@ class ItemsController < ApplicationController
       @cart = Cart.new
   end
 
-
-
-
-private
-    def item_params
+  private
+  
+  def item_params
       params.require(:item).permit(:name, :explanation, :status, :excluded, :genre_id)
-    end
-
-
-
+  end
+    
 end
-
-
