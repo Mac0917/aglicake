0a6307c1cc55177c26e06e26ef96ab385f1bd393
vagrant <vagrant@localhost.localdomain>
2020-05-24 08:17:48 +0000

    ジャンル一覧表記修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,12 @@ class ItemsController < ApplicationController
       @items = Item.page(params[:page]).reverse_order
       @genres = Genre.all
       if params[:genre_id].nil?
+        @genre = Genre.new
+        @genre.name = "商品"
         @items = Item.page(params[:page]).reverse_order
       else
-        genre = Genre.find(params[:genre_id])
-        @items = Item.where(genre_id: genre.id).page(params[:page]).reverse_order
+        @genre = Genre.find(params[:genre_id])
+        @items = Item.where(genre_id: @genre.id).page(params[:page]).reverse_order
       end
    end
 
